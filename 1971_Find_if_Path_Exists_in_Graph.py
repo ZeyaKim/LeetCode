dc925a2453fdf8f201cf19577d51709a75eee226
Zeyakim <chlwltjrdev@gmail.com>
2024-04-22 00:23:43 +0900

    1971_Find_if_Path_Exists_in_Graph 문제에 대한 풀이 변경
    유니온 파인드에 대한 개념 학습 후 풀이법을 유니온 파인드 알고리즘으로 변경해서 유의미한 속도 차이를 보였습니다.

--- 1971_Find_if_Path_Exists_in_Graph.py
+++ 1971_Find_if_Path_Exists_in_Graph.py
@@ -5,30 +5,66 @@ class Solution:
     def validPath(
         self, n: int, edges: List[List[int]], source: int, destination: int
     ) -> bool:
-        graph = {num: [] for num in range(n)}
-        visited = set()
+        class UnionFind:
+            def __init__(self, n, edges):
+                self.parents = [i for i in range(n)]
+                self.ranks = [0] * n
 
-        for edge in edges:
-            graph[edge[0]].append(edge[1])
-            graph[edge[1]].append(edge[0])
+                self.create(edges)
 
-        def visit(cur, reached):
-            if reached or cur in visited:
-                return 0
-            if cur == destination:
-                reached = True
-                return 1
-            visited.add(cur)
+            def root(self, node):
+                parent = self.parents[node]
+                if parent == node:
+                    return parent
 
-            return sum(visit(dest, reached) for dest in graph[cur])
+                return self.root(parent)
 
-        return 0 < visit(source, False)
+            def union(self, start, end):
+                root_start = self.root(start)
+                root_end = self.root(end)
+
+                if root_start == root_end:
+                    return
+
+                match self.compare_rank(root_start, root_end):
+                    case 1:
+                        self.parents[root_end] = self.parents[root_start]
+                    case -1:
+                        self.parents[root_start] = self.parents[root_end]
+                    case 0:
+                        self.parents[root_end] = self.parents[root_start]
+                        self.ranks[root_start] += 1
+
+            def create(self, edges):
+                for start, end in edges:
+                    self.union(start, end)
+
+            def compare_rank(self, start, end):
+                if self.ranks[start] > self.ranks[end]:
+                    return 1
+                elif self.ranks[start] < self.ranks[end]:
+                    return -1
+                else:
+                    return 0
+
+            def is_connected(self, source, destination):
+                return self.root(source) == self.root(destination)
+
+        union_find = UnionFind(n, edges)
+
+        return union_find.is_connected(source, destination)
 
 
 solution = Solution()
 testcases = [
     [3, [[0, 1], [1, 2], [2, 0]], 0, 2],
     [6, [[0, 1], [0, 2], [3, 5], [5, 4], [4, 3]], 0, 5],
+    [
+        10,
+        [[2, 6], [4, 7], [1, 2], [3, 5], [7, 9], [6, 4], [9, 8], [0, 1], [3, 0]],
+        3,
+        5,
+    ],
 ]
 
 for tc in testcases:
